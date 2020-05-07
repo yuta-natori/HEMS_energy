944dc541493cd8a249284cbbabc72e386cd257be
yuta-natori <y-natori@netwrk.co.jp>
2020-05-07 10:46:10 +0900

    viewsファイル切り分け

--- app/urls.py
+++ app/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from . import views
+from .views import index
 
 app_name = 'app'
 urlpatterns = [
-  path('', views.index,  name='index'),
-  path('holiday', views.holiday,  name='holiday'),
-  path('temperature', views.temperature,  name='temperature'),
-  path('electric_power', views.electric_power,  name='electric_power'),
-  path('toukei_keisan', views.toukei_keisan,  name='toukei_keisan'),
-  path('jyuyou_class', views.jyuyou_class, name='jyuyou_class'),
-  path('jyuyou_trend', views.jyuyou_trend, name='jyuyou_trend'),
-  path('jyuyou_modeling', views.jyuyou_modeling, name='jyuyou_modeling'),
-  path('chouki_yosoku', views.chouki_yosoku, name='chouki_yosoku'),
-  path('tanki_yosoku', views.tanki_yosoku, name='tanki_yosoku'),
+  path('', index.index,  name='index'),
+  path('holiday', index.holiday,  name='holiday'),
+  path('temperature', index.temperature,  name='temperature'),
+  path('electric_power', index.electric_power,  name='electric_power'),
+  path('toukei_keisan', index.toukei_keisan,  name='toukei_keisan'),
+  path('jyuyou_class', index.jyuyou_class, name='jyuyou_class'),
+  path('jyuyou_trend', index.jyuyou_trend, name='jyuyou_trend'),
+  path('jyuyou_modeling', index.jyuyou_modeling, name='jyuyou_modeling'),
+  path('chouki_yosoku', index.chouki_yosoku, name='chouki_yosoku'),
+  path('tanki_yosoku', index.tanki_yosoku, name='tanki_yosoku'),
 ]
