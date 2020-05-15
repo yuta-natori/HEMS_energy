861aeaa6a3f07cabdf503b2158850896a78e13ce
ky-tamaki <ky-tamaki@netwrk.co.jp>
2020-05-15 16:23:06 +0900

    需要クラス別解析　集計方法変更　不要なコメント削除

--- app/views/demand_class_views.py
+++ app/views/demand_class_views.py
@@ -11,21 +11,16 @@ import base64
 from app.form.demand_class_form import DemandClassForm
 #model
 from app.models.electricity_data import ElectricityData
+from django.db.models import Sum
 
 def analysis_demand_class(request):    
  
     # グラフ描画処理
     def class_glaph(year, query_data):
         global data_sum,count
-        
-        #data_sum= year.groupby(['user_id'],as_index=False).sum()
-        #data_sum = data_sum[['user_id','total']]
     
-        #data_sum = data_sum.dropna(axis = 0) #欠損　除去
-    
-            #変数の数　count作成
+        #変数の数　count作成
         count = list(range(1,query_data.count()+1))
-        #data_sum['count'] = count #countを追加
 
         global fig,plot
         #年間
@@ -35,19 +30,19 @@ def analysis_demand_class(request):
     
         #散布図　値指定
         fig=plt.figure(figsize = (7, 5)) #fig=　pdf出力のため
-        plot=plt.scatter(count, query_data.values_list('total'),s = 20)
+        plot=plt.scatter(count, query_data.values_list('total_sum'),s = 20)
     
         #タイトル　ラベル
         plt.title('%s年　需要家別電力需要量分布(年間)'%year,fontsize = 15) # タイトル
-        plt.xlabel('需要家番号 \n 地域：%s'%region, fontsize = 12) # x軸ラベル　#サブタイトル
+        plt.xlabel('需要家番号 \n 地域：%s'%area, fontsize = 12) # x軸ラベル　#サブタイトル
         plt.ylabel('電力需要量(kWh)', fontsize = 12) # y軸ラベル
         plt.grid(False) # (8)目盛線の表示
     
         #軸目盛　間隔　
         plt.xlim(0,len(count))
         plt.xticks(fontsize = 11)
-        plt.ylim(0, max(query_data.values_list('total', flat=True))+1000)
-        plt.yticks(np.arange(0, max(query_data.values_list('total', flat=True))+1000, 2500),fontsize = 11,rotation=90)
+        plt.ylim(0, max(query_data.values_list('total_sum', flat=True))+1000)
+        plt.yticks(np.arange(0, max(query_data.values_list('total_sum', flat=True))+1000, 2500),fontsize = 11,rotation=90)
         plt.tick_params(length = 3) #仮
     
         #クラス分け　ライン(横)
@@ -76,11 +71,11 @@ def analysis_demand_class(request):
         
         #ヒストグラム　値指定
         fig_ = plt.figure(figsize=(7, 6))
-        plot = plt.hist(query_data.values_list('total', flat=True),bins=16,range=(0, 16000),ec='black')
+        plot = plt.hist(query_data.values_list('total_sum', flat=True),bins=16,range=(0, 16000),ec='black')
         
         #タイトル　ラベル
         plt.title('電力需要量―需要家ヒストグラム%s年'%year,fontsize = 15) # タイトル
-        plt.xlabel('年間電力需要量 (kWh)\n 地域：%s'%region, fontsize = 12) # x軸ラベル　#サブタイトル
+        plt.xlabel('年間電力需要量 (kWh)\n 地域：%s'%area, fontsize = 12) # x軸ラベル　#サブタイトル
         plt.ylabel('需要家数', fontsize = 12) # y軸ラベル
         plt.grid(False) # (8)目盛線の表示
     
@@ -152,30 +147,12 @@ def analysis_demand_class(request):
         e_date = str(int(select_year) + 1) + '/02/28' #TODO: うるう年の判定
         
     #地域名
-    #region_= 'shinyoko' #TODO: 地域名から地域ID的なものを取得
     area = request.POST['area']
     
-    #読み込みファイル指定　既存のもの
-    #data_base = pd.read_sql_query("SELECT user_id, date, hour, minute, total from %s_data where date between '%s' and '%s'"%(region_,s_date,e_date),cnt)
-    data_base = ElectricityData.objects.filter(
+    data_base = ElectricityData.objects.values('household_id').filter(
         date__range = (s_date, e_date),
         area = area
-    )
-    print("data_base-------------------------↓")
-    print(data_base)
-    print(data_base.values())
-    print(data_base.values_list('total', flat=True).first() )
-    print(data_base.count())
-    
-    #年　抽出
-    #data_base['year']=data_base['date'].astype(str).str[:4]    #data_baseに追加 
-    #月　抽出
-    #data_base['month']=data_base['date'].astype(str).str[5:7]  #data_baseに追加
-
-    
-    #年の要素数　地域名
-    #data_year = data_base.date.astype(str).str[:4]
-    region = '新横浜'
+        ).annotate(total_sum=Sum('total'))
 
     #画像取得
     distribution, histogram = class_glaph(select_year, data_base)
