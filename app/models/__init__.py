8458fc6496ac9697cd6cd56237d8918fcd61a6c1
yuta-natori <y-natori@netwrk.co.jp>
2020-05-18 16:10:26 +0900

    energy_data_after_screeningモデル追加

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,4 @@ from app.models.electricity_data import ElectricityData
 from app.models.holiday_coefficient import HolidayCoefficient
 from app.models.temperature_variation_coefficient import TemperatureVariationCoefficient
 from app.models.power_demand_profile import PowerDemandProfile
+from app.models.energy_data_after_screening import EnergyDataAfterScreening
---
+++ app/models/energy_data_after_screening.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class EnergyDataAfterScreening(models.Model):
+    household_id = models.CharField(max_length=20)
+    date = models.CharField(max_length=10)
+    hour = models.BigIntegerField()
+    minute = models.BigIntegerField()
+    total = models.FloatField()
+    val1 = models.FloatField()
+    val2 = models.FloatField()
+    val3 = models.FloatField()
+    val4 = models.FloatField()
+    val5= models.FloatField()
+    val6 = models.FloatField()
+    val7 = models.FloatField()
+    val8 = models.FloatField()
+    val9 = models.FloatField()
+    val10 = models.FloatField()
+    
+    class Meta:
+        db_table = 't_energy_data_after_screening'
+        unique_together = ('household_id', 'date', 'hour', 'minute')
