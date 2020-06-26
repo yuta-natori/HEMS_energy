325327707eb1526863b56fb5177116e51dd95e6d
ky-tamaki <ky-tamaki@netwrk.co.jp>
2020-06-26 13:51:25 +0900

    トレンド分析画面　Form適用

--- app/form/trend_analysis_form.py
+++ app/form/trend_analysis_form.py
@@ -52,9 +52,69 @@ CHOICE_WEEK = (
     (4, 4)
 )
 
+CHOICE_DISPLAY = (
+    ("daily", "日毎トレンド分析"),
+    ("weekly", "週刊トレンド分析"),
+    ("monthly", "月間トレンド分析"),
+    ("yearly", "年間トレンド分析"),
+    ("temp", "外気温需要トレンド分析")
+)
+
 class TrendAnalysisFormClass(forms.Form):
     area = forms.CharField(
-        label='地域の指定',
         max_length=50,
         required=True,
+    )
+    
+    fromYear = forms.ChoiceField(
+        choices=CHOICE_YEARS,
+    )
+    
+    toYear = forms.ChoiceField(
+        choices=CHOICE_YEARS
+    )
+    
+    dailyTrend = forms.CharField(
+        max_length=10,
+        required=False,
+    )
+    
+    weeklyYear = forms.ChoiceField(
+        choices=CHOICE_YEARS,
+        required=False,
+    )
+    
+    weeklyMonth = forms.ChoiceField(
+        choices=CHOICE_MONTH,
+        required=False,
+    )
+    
+    weeklyWeek = forms.ChoiceField(
+        choices=CHOICE_WEEK,
+        required=False,
+    )
+    
+    yearlyYear = forms.ChoiceField(
+        choices=CHOICE_YEARS,
+        required=False,
+    )
+    
+    yearlyMonth = forms.ChoiceField(
+        choices=CHOICE_MONTH,
+        required=False,
+    )
+    
+    maxLine = forms.IntegerField(
+        required=False,
+    )
+    
+    minLine = forms.IntegerField(
+        required=False,
+    )
+    
+    trendDisplay = forms.MultipleChoiceField(
+        required=True,
+        initial=["daily"],
+        widget=forms.RadioSelect,
+        choices=CHOICE_DISPLAY,
     )
